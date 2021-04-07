77a8560cdf843b00c9e3cfc79fcd1e9dca1315bb
Patrick Carty <carty.patrick@gmail.com>
2021-04-07 11:41:18 +1000

    Added scaffolded identity code

--- Whirlpool/Startup.cs
+++ Whirlpool/Startup.cs
@@ -24,7 +24,6 @@ namespace Whirlpool
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,7 +36,6 @@ namespace Whirlpool
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -48,7 +46,6 @@ namespace Whirlpool
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
