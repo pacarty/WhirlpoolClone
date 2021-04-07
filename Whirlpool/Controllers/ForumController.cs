150e440dbb04b9fdb97629f12ae0c9f823d34057
Patrick Carty <carty.patrick@gmail.com>
2021-04-07 12:14:16 +1000

    Create Forum page and controller

---
+++ Whirlpool/Controllers/ForumController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Whirlpool.Models;
+
+namespace Whirlpool.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ForumController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
