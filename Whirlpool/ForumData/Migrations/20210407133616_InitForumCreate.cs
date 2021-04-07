6b0deca741dc273339a87a22e0df69e5e69b177c
Patrick Carty <carty.patrick@gmail.com>
2021-04-07 23:40:56 +1000

    Forum migrations added

---
+++ Whirlpool/ForumData/Migrations/20210407133616_InitForumCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Whirlpool.ForumData.Migrations
+{
+    public partial class InitForumCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubForums",
+                columns: table => new
+                {
+                    SubForumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubForums", x => x.SubForumId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubForums");
+        }
+    }
+}
