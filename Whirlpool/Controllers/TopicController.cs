7a60ac776a9c2ffb731ef874319df3f5bb99e34e
Patrick Carty <carty.patrick@gmail.com>
2021-04-11 06:11:19 +1000

    Added Topic routing and controller

---
+++ Whirlpool/Controllers/TopicController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Whirlpool.ForumData;
+using Whirlpool.Models;
+using Whirlpool.ViewModels;
+
+namespace Whirlpool.Controllers
+{
+    public class TopicController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ForumContext _context;
+
+        public TopicController(
+            ILogger<HomeController> logger,
+            ForumContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index(int id)
+        {
+            Topic topic = _context.Topics.Find(id);
+            TopicVM topicVM = new TopicVM
+            {
+                TopicViewName = topic.TopicName
+            };
+
+            return View(topicVM);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Whirlpool/ViewModels/TopicVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whirlpool.ViewModels
+{
+    public class TopicVM
+    {
+        public string TopicViewName { get; set; }
+    }
+}
