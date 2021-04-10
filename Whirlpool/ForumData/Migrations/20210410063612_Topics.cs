ff355a2ad7fe66d4285af019011d1aa1cffb8872
Patrick Carty <carty.patrick@gmail.com>
2021-04-10 16:39:18 +1000

    Applied Topics migration

---
+++ Whirlpool/ForumData/Migrations/20210410063612_Topics.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Whirlpool.ForumData.Migrations
+{
+    public partial class Topics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    TopicId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TopicName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubForumId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.TopicId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Topics");
+        }
+    }
+}
